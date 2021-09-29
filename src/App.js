c5e71563b98429d36233ef94482d0fe66276909b
Sándor Dési <d.alex.galaxy@gmail.com>
2021-09-29 10:46:45 +0100

    add alan

--- src/App.js
+++ src/App.js
@@ -7,16 +7,24 @@ const alanKey = '22b36abd0e2e1ac15e09fb0554a7f2b12e956eca572e1d8b807a3e2338fdd0d
 
 const App = ()=>{
 
-  useEffect(()=> {
-    alanBtn({
-      key: alanKey,
-      onCommand: ({command})=> {
-        if(command === 'testCommand') {
-          alert('This command was executed')
-        }
-      }
-    })
-  },[])
+  useEffect(() => {
+    let alanBtnInstance = alanBtn({
+        key: alanKey,
+        onCommand: (commandData) => {
+            if (commandData.command === 'go:back') {
+                // Call the client code that will react to the received command
+            }
+        },
+        onConnectionStatus: async function(status) {
+            if (status === 'authorized') {
+                await alanBtnInstance.activate();
+                alanBtnInstance.playText('Hey, this is Alan');
+            }
+        },
+    });
+}, []);
+
+  
 
   return (
     <div><h1>
